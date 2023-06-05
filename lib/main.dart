4b8b8c39e4fae1b25776229dfef16850317bcd25
Mont38 <montsetapia848@gmail.com>
2023-06-05 12:46:52 -0600

    a

--- lib/main.dart
+++ lib/main.dart
@@ -24,6 +24,12 @@ class MyApp extends StatelessWidget {
   }
 }
 
+final _offercontroller = TextEditingController();
+final candidatecontroller = TextEditingController();
+final candidateUsercontroller = TextEditingController();
+final sdpController = TextEditingController();
+bool bandera = false;
+
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
 
@@ -36,7 +42,150 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   final _localVideoRenderer = RTCVideoRenderer();
   final _remoteVideoRenderer = RTCVideoRenderer();
-  final sdpController = TextEditingController();
+
+  final txtoffer = TextFormField(
+    controller: _offercontroller,
+    maxLines: 2,
+    maxLength: TextField.noMaxLength,
+    decoration: InputDecoration(
+      labelText: 'codigo offer',
+      labelStyle: TextStyle(
+          color: Color.fromRGBO(
+            244,
+            244,
+            244,
+            1,
+          ),
+          fontSize: 13),
+      border: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(10),
+        borderSide: BorderSide(
+          color: Colors.grey.shade400,
+          width: 2,
+        ),
+      ),
+      focusedBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(10),
+        borderSide: const BorderSide(
+          color: Color.fromRGBO(255, 178, 122, 1),
+          width: 2,
+        ),
+      ),
+      filled: true,
+      fillColor: Color.fromARGB(126, 103, 66, 106),
+      errorBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(10),
+        borderSide: const BorderSide(
+          color: Color.fromRGBO(66, 71, 106, 0.5),
+          width: 2,
+        ),
+      ),
+      contentPadding: EdgeInsets.symmetric(vertical: 16, horizontal: 12),
+    ),
+  );
+  final txtremote = TextFormField(
+    controller: sdpController,
+    maxLines: 4,
+    maxLength: TextField.noMaxLength,
+    decoration: InputDecoration(
+      labelText: 'remoto',
+      hintText: 'inserta codigo',
+      labelStyle:
+          TextStyle(color: Color.fromRGBO(244, 244, 244, 1), fontSize: 13),
+      border: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(10),
+        borderSide: BorderSide(
+          color: Colors.grey.shade400,
+          width: 2,
+        ),
+      ),
+      focusedBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(10),
+        borderSide: const BorderSide(
+          color: Color.fromRGBO(255, 178, 122, 1),
+          width: 2,
+        ),
+      ),
+      filled: true,
+      fillColor: const Color.fromRGBO(66, 71, 106, 0.5),
+      errorBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(10),
+        borderSide: const BorderSide(
+          color: Color.fromRGBO(66, 71, 106, 0.5),
+          width: 2,
+        ),
+      ),
+      contentPadding: EdgeInsets.symmetric(vertical: 16, horizontal: 12),
+    ),
+  );
+  final txtCandidateremote = TextFormField(
+    controller: candidatecontroller,
+    maxLines: 4,
+    maxLength: TextField.noMaxLength,
+    decoration: InputDecoration(
+      labelText: 'remoto candidato',
+      hintText: 'inserta codigo',
+      labelStyle:
+          TextStyle(color: Color.fromRGBO(244, 244, 244, 1), fontSize: 13),
+      border: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(10),
+        borderSide: BorderSide(
+          color: Colors.grey.shade400,
+          width: 2,
+        ),
+      ),
+      focusedBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(10),
+        borderSide: const BorderSide(
+          color: Color.fromRGBO(255, 178, 122, 1),
+          width: 2,
+        ),
+      ),
+      filled: true,
+      fillColor: const Color.fromRGBO(66, 71, 106, 0.5),
+      errorBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(10),
+        borderSide: const BorderSide(
+          color: Color.fromRGBO(66, 71, 106, 0.5),
+          width: 2,
+        ),
+      ),
+      contentPadding: EdgeInsets.symmetric(vertical: 16, horizontal: 12),
+    ),
+  );
+  final txtcandidate = TextFormField(
+    controller: candidateUsercontroller,
+    maxLines: 2,
+    maxLength: TextField.noMaxLength,
+    decoration: InputDecoration(
+      labelText: 'candidato',
+      labelStyle: TextStyle(color: Color.fromRGBO(244, 244, 244, 1)),
+      border: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(10),
+        borderSide: BorderSide(
+          color: Colors.grey.shade400,
+          width: 2,
+        ),
+      ),
+      focusedBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(10),
+        borderSide: const BorderSide(
+          color: Color.fromRGBO(255, 178, 122, 1),
+          width: 2,
+        ),
+      ),
+      filled: true,
+      fillColor: const Color.fromRGBO(66, 71, 106, 0.5),
+      errorBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(10),
+        borderSide: const BorderSide(
+          color: Color.fromRGBO(66, 71, 106, 0.5),
+          width: 2,
+        ),
+      ),
+      contentPadding: EdgeInsets.symmetric(vertical: 16, horizontal: 12),
+    ),
+  );
 
   bool _offer = false;
 
@@ -65,6 +214,7 @@ class _MyHomePageState extends State<MyHomePage> {
 
   _createPeerConnecion() async {
     Map<String, dynamic> configuration = {
+      'sdpSemantics': 'plan-b',
       "iceServers": [
         {"url": "stun:stun.l.google.com:19302"},
       ]
@@ -92,6 +242,29 @@ class _MyHomePageState extends State<MyHomePage> {
           'sdpMid': e.sdpMid.toString(),
           'sdpMlineIndex': e.sdpMLineIndex,
         }));
+
+        if (bandera == true) {
+          final guardar = json.encode({
+            'candidate': e.candidate.toString(),
+            'sdpMid': e.sdpMid.toString(),
+            'sdpMlineIndex': e.sdpMLineIndex,
+          });
+
+          Map<String, dynamic> mapa = json.decode(guardar);
+
+          // Acceder a los valores del mapa
+          String candidate = mapa['candidate'];
+          String sdpMid = mapa['sdpMid'];
+          int sdpMlineIndex = mapa['sdpMlineIndex'];
+
+          // Imprimir los valores
+          print('Candidate: $candidate');
+          print('sdpMid: $sdpMid');
+          print('sdpMlineIndex: $sdpMlineIndex');
+          if (candidate.contains('network-id 2') && candidate.contains('udp')) {
+            candidateUsercontroller.text = guardar;
+          }
+        }
       }
     };
 
@@ -112,19 +285,24 @@ class _MyHomePageState extends State<MyHomePage> {
         await _peerConnection!.createOffer({'offerToReceiveVideo': 1});
     var session = parse(description.sdp.toString());
     print(json.encode(session));
+
     _offer = true;
+    _offercontroller.text = json.encode(session);
 
     _peerConnection!.setLocalDescription(description);
   }
 
   void _createAnswer() async {
+    bandera = true;
     RTCSessionDescription description =
         await _peerConnection!.createAnswer({'offerToReceiveVideo': 1});
 
     var session = parse(description.sdp.toString());
-    print(json.encode(session));
+    print("${json.encode(session)}iuwuwu");
 
-    _peerConnection!.setLocalDescription(description);
+    _peerConnection!
+        .setLocalDescription(description)
+        .then((value) => candidatecontroller.text = json.encode((session)));
   }
 
   void _setRemoteDescription() async {
@@ -135,7 +313,7 @@ class _MyHomePageState extends State<MyHomePage> {
 
     RTCSessionDescription description =
         RTCSessionDescription(sdp, _offer ? 'answer' : 'offer');
-    print(description.toMap());
+    print("uw${description.toMap()}");
 
     await _peerConnection!.setRemoteDescription(description);
   }
@@ -191,59 +369,158 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        backgroundColor: Color.fromARGB(187, 204, 172, 218),
         appBar: AppBar(
-          title: Text(widget.title),
-        ),
-        body: Column(
-          children: [
-            videoRenderers(),
-            Row(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: SizedBox(
-                    width: MediaQuery.of(context).size.width * 0.5,
-                    child: TextField(
-                      controller: sdpController,
-                      keyboardType: TextInputType.multiline,
-                      maxLines: 4,
-                      maxLength: TextField.noMaxLength,
+            title: const Text("WebRTC"),
+            centerTitle: true,
+            elevation: 1,
+            backgroundColor: const Color.fromARGB(255, 230, 191, 232),
+            shape: const RoundedRectangleBorder(
+                borderRadius: BorderRadius.vertical(
+              bottom: Radius.circular(30),
+            ))),
+        body: Container(
+          decoration:
+              const BoxDecoration(color: Color.fromARGB(187, 204, 172, 218)),
+          child: Column(
+            children: [
+              videoRenderers(),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Column(
+                      children: [
+                        ElevatedButton(
+                          onPressed: _createOffer,
+                          style: ElevatedButton.styleFrom(
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(50),
+                              ),
+                              padding: const EdgeInsets.all(20.0),
+                              backgroundColor:
+                                  const Color.fromARGB(255, 230, 191, 232)),
+                          child: const Icon(Icons.person,
+                              size: 15, color: Colors.black),
+                        ),
+                        const SizedBox(height: 3.0),
+                        const Text('Offer'),
+                        const SizedBox(height: 8.0),
+                      ],
                     ),
                   ),
-                ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    ElevatedButton(
-                      onPressed: _createOffer,
-                      child: const Text("Offer"),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      children: [
+                        ElevatedButton(
+                          onPressed: _createAnswer,
+                          style: ElevatedButton.styleFrom(
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(50),
+                              ),
+                              padding: const EdgeInsets.all(20.0),
+                              backgroundColor:
+                                  Color.fromARGB(255, 230, 191, 232)),
+                          child: const Icon(Icons.people,
+                              size: 15, color: Colors.black),
+                        ),
+                        const SizedBox(height: 3.0),
+                        const Text('Answer'),
+                        const SizedBox(height: 8.0),
+                      ],
                     ),
-                    const SizedBox(
-                      height: 10,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      children: [
+                        ElevatedButton(
+                          onPressed: _setRemoteDescription,
+                          style: ElevatedButton.styleFrom(
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(50),
+                              ),
+                              padding: const EdgeInsets.all(20.0),
+                              backgroundColor:
+                                  Color.fromARGB(255, 230, 191, 232)),
+                          child: const Icon(Icons.add_to_home_screen_sharp,
+                              size: 15, color: Colors.black),
+                        ),
+                        const SizedBox(height: 3.0),
+                        const Text('set remote'),
+                        const SizedBox(height: 8.0),
+                        const SizedBox(
+                          height: 3,
+                        ),
+                      ],
                     ),
-                    ElevatedButton(
-                      onPressed: _createAnswer,
-                      child: const Text("Answer"),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      children: [
+                        ElevatedButton(
+                          onPressed: _addCandidate,
+                          style: ElevatedButton.styleFrom(
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(50),
+                              ),
+                              padding: const EdgeInsets.all(20.0),
+                              backgroundColor:
+                                  Color.fromARGB(255, 230, 191, 232)),
+                          child: const Icon(Icons.person_add_alt_rounded,
+                              size: 15, color: Colors.black),
+                        ),
+                        const SizedBox(height: 3.0),
+                        const Text('add candidate'),
+                        const SizedBox(height: 8.0),
+                      ],
                     ),
-                    const SizedBox(
-                      height: 10,
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text("offer"),
+                        SizedBox(
+                          width: MediaQuery.of(context).size.width * 0.3,
+                          child: txtoffer,
+                        ),
+                      ],
                     ),
-                    ElevatedButton(
-                      onPressed: _setRemoteDescription,
-                      child: const Text("Set Remote Description"),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      children: [
+                        Text("candidato"),
+                        SizedBox(
+                          width: MediaQuery.of(context).size.width * 0.3,
+                          child: txtCandidateremote,
+                        ),
+                        SizedBox(
+                          width: MediaQuery.of(context).size.width * 0.3,
+                          child: txtcandidate,
+                        ),
+                      ],
                     ),
-                    const SizedBox(
-                      height: 10,
-                    ),
-                    ElevatedButton(
-                      onPressed: _addCandidate,
-                      child: const Text("Set Candidate"),
-                    ),
-                  ],
-                )
-              ],
-            ),
-          ],
+                  ),
+                ],
+              ),
+              SizedBox(
+                width: MediaQuery.of(context).size.width * 0.3,
+                child: txtremote,
+              ),
+            ],
+          ),
         ));
   }
 }
